090442bd6d56c528d7b1d87653da14ca0b5bb48c
frahlg <fredrik@ahlgren.nu>
2017-04-03 16:44:11 +0200

    nr2

--- data import/data_cleanup.py
+++ data import/data_cleanup.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import numpy as np
+import timeit
 # Import all data into a DataFrame
 #
-df = pd.read_excel('all_data.xlsx',index_col=0)
+
+data_path = '/Users/fredde/Dropbox/arbetsprojekt/Birka_data/'
+
+%timeit df = pd.read_excel(data_path + 'ME.xlsx',index_col=0)
+
+df.to_hdf('ME.h5','table',append=True)
 
 
 # Exploring the dataset
@@ -14,3 +20,4 @@ print(str(len(list(df))) + ' number of columns')
 # It contains 183 datapoints, with a 15-minute interval of logging.
 
 #%%
+1+1
